4e71b4611f367080ddf74c891b68ca4cceb6ed8c
makendym <makendymidouin99@gmail.com>
2025-05-21 15:39:48 +0000

    I have fixed the issue with Tempo and now all the functions are working properly.

--- next.config.js
+++ next.config.js
@@ -9,9 +9,10 @@ const nextConfig = {
 };
 
 if (process.env.NEXT_PUBLIC_TEMPO && !isProd) {
-  nextConfig.experimental = {
-    swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]],
-  };
+  // Temporarily disable SWC plugins to fix build issues
+  // nextConfig.experimental = {
+  //   swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]],
+  // };
 }
 
 module.exports = nextConfig;
